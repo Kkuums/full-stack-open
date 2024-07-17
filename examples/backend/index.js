7ec2068fc0836dbae1f1d71357381d95b3ce31dc
Kristiāns Kūms <kristians.kums@gmail.com>
2024-07-17 17:07:05 +0300

    part3b

--- examples/backend/index.js
+++ examples/backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 let notes = [
   {
@@ -20,6 +22,16 @@ let notes = [
     important: true,
   },
 ];
+
+const requestLogger = (request, response, next) => {
+  console.log("Method", request.method);
+  console.log("Path", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(requestLogger);
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -72,6 +84,13 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "uknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
